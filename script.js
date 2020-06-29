7637a42682139dac1b7d2582c0f118deb3a32164
sid <siddharthagoel1998@gmail.com>
2020-06-29 15:30:52 +0530

    home page v4

--- script.js
+++ script.js
@@ -9,13 +9,15 @@ function bodyScroll() {
     for (let i = 0; i < x.length; i++) {
       x[i].style.color = "black";
       btn.className = "btn1";
-      nav.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+      nav.style.backgroundColor = "rgba(255, 255, 255, 1)";
+      nav.style.boxShadow = "0px 4px 6px rgba(0, 0, 0, 0.15)"
     }
   } else if (screen.width > 900) {
     for (let i = 0; i < x.length; i++) {
       x[i].style.color = "white";
       btn.className = "btn2";
       nav.style.backgroundColor = "transparent";
+      nav.style.boxShadow = "none"
     }
   }
 }
