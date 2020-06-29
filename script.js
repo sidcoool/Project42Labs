50943ee8d9e6560fa1548ab917572c716fe74483
sid <siddharthagoel1998@gmail.com>
2020-06-29 13:14:21 +0530

    home page v3

---
+++ script.js
@@ -0,0 +1,36 @@
+function bodyScroll() {
+  console.log(screen.width);
+  let x = document.getElementsByClassName("sChange");
+  let btn = document.getElementById("sChange");
+  let nav = document.getElementById("navBack");
+
+  if (window.pageYOffset > 340) {
+    // console.log(x)
+    for (let i = 0; i < x.length; i++) {
+      x[i].style.color = "black";
+      btn.className = "btn1";
+      nav.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+    }
+  } else if (screen.width > 900) {
+    for (let i = 0; i < x.length; i++) {
+      x[i].style.color = "white";
+      btn.className = "btn2";
+      nav.style.backgroundColor = "transparent";
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (screen.width < 900) {
+    console.log(screen.width);
+    let x = document.getElementsByClassName("sChange");
+    let btn = document.getElementById("sChange");
+    let nav = document.getElementById("navBack");
+
+    for (let i = 0; i < x.length; i++) {
+      x[i].style.color = "black";
+      btn.className = "btn1";
+      nav.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+    }
+  }
+});
