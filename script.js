497a879106c42c5be8c6aa5bc4bff08c684830c6
sid <siddharthagoel1998@gmail.com>
2020-07-08 14:32:46 +0530

    mobile navbar changes

--- script.js
+++ script.js
@@ -35,6 +35,36 @@ document.addEventListener("DOMContentLoaded", () => {
       nav.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
     }
   }
+
+
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementsByClassName("navbar-toggler")[0];
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close1")[0];
+
+  var mcom = document.getElementsByClassName("modal-content1")[0];
+
+  // When the user clicks the button, open the modal 
+  btn.onclick = function () {
+    modal.style.display = "block";
+    mcom.style.animation = "navSlider 0.5s linear";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 });
 
 
@@ -51,7 +81,7 @@ function onScroll2() {
 
 
 function arrowToggle(e) {
-  
+
   if (e.className.includes("down")) {
     e.className = "fas fa-chevron-up font-23 color-red"
   }
@@ -59,3 +89,5 @@ function arrowToggle(e) {
     e.className = "fas fa-chevron-down font-23 color-red"
   }
 }
+
+
